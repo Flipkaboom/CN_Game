956f7c3717a561364a0dcf73185bc228620073fd
Flip <p.a.j.grim@student.vu.nl>
2023-05-14 19:46:38 +0200

    Finished basic from of Operation class and the operations themselves (the subclasses). No operations with parameters have been implemented yet

--- GameNetworkProtocol/operations.py
+++ GameNetworkProtocol/operations.py
@@ -1,25 +1,118 @@
-#TODO make abstract?
 from abc import ABC, abstractmethod
 
+op_num_counter = 0
 
 class Operation(ABC):
     time:int = 0 #TODO initialize or no?
 
+    def __init_subclass__(cls, **kwargs):
+        global op_num_counter
+        cls.num = op_num_counter
+        op_num_counter += 1
+
     @property
     @abstractmethod
-    def num(self) -> int:
+    def length(self) -> int:
         raise NotImplementedError
 
-    @abstractmethod
-    def encode(self):
-        raise NotImplementedError
+    def encode(self) -> bytes:
+        return self.num.to_bytes(self.length, 'big')
 
-op_list:list[Operation] = list[Operation](
+    @classmethod
+    def from_data(cls, data:bytes):
+        return cls()
 
-)
+    @staticmethod
+    def from_num(num:int, data:bytes = bytes()):
+        cls = Operation.__subclasses__()[num]
+        return cls.from_data(data)
+
 
 class Pos(Operation):
-    num = 0
+    #use init to make new instance with parameters
+    def __init__(self):
+        pass
 
     def encode(self):
+        pass
+
+    @classmethod
+    def from_data(cls, data:bytes):
+        pass
+
+
+class Attack(Operation):
+    def __init__(self):
+        pass
+
+    def encode(self):
+        pass
+
+    @classmethod
+    def from_data(cls, data:bytes):
+        pass
+
+class Hit(Operation):
+    def __init__(self):
+        pass
+
+    def encode(self):
+        pass
+
+    @classmethod
+    def from_data(cls, data:bytes):
+        pass
+
+class BlockStart(Operation):
+    length = 1
+
+class BlockStop(Operation):
+    length = 1
+
+class Jump(Operation):
+    def __init__(self):
+        pass
+
+    def encode(self):
+        pass
+
+    @classmethod
+    def from_data(cls, data:bytes):
+        pass
+
+class DodgeStart(Operation):
+    length = 1
+
+class DodgeStop(Operation):
+    length = 1
+
+class Death(Operation):
+    length = 1
+
+class MatchReady(Operation):
+    length = 1
+
+class MatchUnready(Operation):
+    length = 1
+
+class ConnRequest(Operation):
+    def __init__(self):
+        pass
+
+    def encode(self):
+        pass
+
+    @classmethod
+    def from_data(cls, data:bytes):
+        pass
+
+class OtherPlayer(Operation):
+    def __init__(self):
+        pass
+
+    def encode(self):
+        pass
+
+    @classmethod
+    def from_data(cls, data:bytes):
         pass
